e48fe011138b90ca31aaf0e43f386e99d987db8b
Konstantins <k.7vlasov@gmail.com>
2021-04-02 22:30:26 +0100

    added video fashion

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
