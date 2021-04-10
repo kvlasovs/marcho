aed57c9003ea685dda87af29afd55a432cc7384e
Konstantins <k.7vlasov@gmail.com>
2021-04-10 11:31:09 +0100

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,32 @@
 $(function(){
+
+  $('.shop-content__filter-btn').on('click', function() {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active')
+  });
+
+  $('.button-list').on('click', function() {
+    $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function() {
+    $('.product-item').removeClass('product-item--list');
+  });
+
+  $('.select-style').styler()
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    }
+  });
   
   $('.top-slider__inner').slick({
     dots: true,
@@ -57,6 +85,7 @@ $(function(){
   
   const deadline = $('.promo__clock').attr('data-time');
   initializeClock('promo__clock', deadline);
+
 
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,8 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
